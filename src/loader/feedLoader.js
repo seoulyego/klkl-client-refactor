a0f427d34d3844cb0739520f0fc24ba983919eee
seoulyego <youngsk786@gmail.com>
2025-04-11 03:50:17 +0900

    refactor: asynchronize network requests from loaders #1
    아래 loader들의 네트워크 요청을 비동기로 처리합니다.
    - feedLoader
    - homeLoader
    - productEditLoader

--- src/loader/feedLoader.js
+++ src/loader/feedLoader.js
@@ -1,12 +1,22 @@
 import kyInstance from '@utils/kyInstance'
 
 const feedLoader = async () => {
-  try {
-    const regionData = await kyInstance.get('regions/hierarchy').json()
-    const categoryData = await kyInstance.get('categories/hierarchy').json()
-    return { regionData, categoryData }
-  } catch (error) {
+  const regionRequest = kyInstance.get('regions/hierarchy').json()
+  const categoryRequest = kyInstance.get('categories/hierarchy').json()
+
+  const responseData = await Promise.all([
+    regionRequest,
+    categoryRequest,
+  ]).catch((error) => {
     throw Error(`${error.response.status}`)
+  })
+
+  console.log(`regionData: ${responseData[0]}`)
+  console.log(`categoryData: ${responseData[1]}`)
+
+  return {
+    regionData: responseData[0],
+    categoryData: responseData[1],
   }
 }
 
